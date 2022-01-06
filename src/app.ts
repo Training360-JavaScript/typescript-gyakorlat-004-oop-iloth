9fc48c6e7d1a101a5f52724d7e522b83807b780d
iloth <loth.istvan@hotmail.com>
2022-01-06 20:06:45 +0100

    Solution

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 // Importáld be a HumanHero és TransformerHero osztályokat.
-
+import { HumanHero } from "./hero";
+import { TransformerHero } from "./transformer";
 
 /**
  * Állítsd be helyesen a változót!
@@ -8,7 +9,11 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+export const humans: HumanHero[] = [
+  new HumanHero(1, "1", "0", 0, 0),
+  new HumanHero(2, "1", "0", 0, 0),
+  new HumanHero(3, "1", "0", 0, 0),
+];
 
 /**
  * Állítsd be helyesen a változót!
@@ -17,4 +22,8 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} transformers
  */
-export const transformers: any[] = [];
+export const transformers: TransformerHero[] = [
+  new TransformerHero(1, "2", 1, 1, ""),
+  new TransformerHero(2, "2", 1, 1, ""),
+  new TransformerHero(3, "2", 1, 1, ""),
+];
--- src/hero.ts
+++ src/hero.ts
@@ -7,6 +7,20 @@ import { IHuman } from './hero-interface';
  * Deklaráld benne az interfész által megkövetelt változókat.
  * Definiáld a konstruktort és kérj be minden változót, majd állítsd be őket.
  */
-export class HumanHero {
+export class HumanHero implements IHuman {
 
+  constructor(id: number, name: string, sex: string, age: number, health: number) {
+    this.id = id;
+    this.name = name;
+    this.sex = sex;
+    this.age = age;
+    this.health = health;
+  }
+
+  id: number;
+  name: string;
+  sex: string;
+  age: number;
+  health: number;
+  photo?: string;
 }
